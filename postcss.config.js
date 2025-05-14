9394f3e044f4555b62918c5776fcff6d7248f39b
Maria Shkurko <Angel-of-Death.Maria@yandex.ru>
2025-05-15 01:10:32 +0300

    postcss setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
-import postcssPresetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
+import postcssPresetEnv from 'postcss-preset-env';
+import stylelint from 'stylelint';
 
 export default {
     plugins: [
+        stylelint(),
         autoprefixer(),
+        postcssNesting(),
         postcssPresetEnv(),
     ],
 };
