0f680f658bca13b297069a545c608c5f7325ce86
Maria Shkurko <Angel-of-Death.Maria@yandex.ru>
2025-05-26 02:23:01 +0300

    components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Avatar } from "./avatar";
+export { Button } from "./button";
+export { Input } from "./input";
+export { ChatItem } from "./chatItem";
+export { CorrespondenceFeed, EmptyFeed } from "./correspondenceFeed";
