a5878b4caf5bec4008215036a2f892e2390c50ea
Maria Shkurko <Angel-of-Death.Maria@yandex.ru>
2025-06-07 23:43:32 +0300

    add api

---
+++ src/api/HTTP.ts
@@ -0,0 +1,83 @@
+const METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+} as const;
+
+type HTTPMethod = keyof typeof METHODS;
+
+type Headers = Record<string, string>;
+
+interface RequestOptions {
+  method?: HTTPMethod;
+  headers?: Headers;
+  data?: any;
+  timeout?: number;
+}
+
+function queryStringify(data: Record<string, any>): string {
+  const keys = Object.keys(data);
+  return keys.length
+    ? '?' + keys.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`).join('&')
+    : '';
+}
+
+class HTTP {
+  get = (url: string, options: RequestOptions = {}) => {
+    return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
+  };
+
+  post = (url: string, options: RequestOptions = {}) => {
+    return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+  };
+
+  put = (url: string, options: RequestOptions = {}) => {
+    return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+  };
+
+  delete = (url: string, options: RequestOptions = {}) => {
+    return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+  };
+
+  request = (url: string, options: RequestOptions, timeout: number = 5000): Promise<XMLHttpRequest> => {
+    const { method, data, headers = {} } = options;
+
+    return new Promise((resolve, reject) => {
+      if (!method) {
+        reject(new Error('No method specified'));
+        return;
+      }
+
+      const xhr = new XMLHttpRequest();
+      const isGet = method === METHODS.GET;
+      xhr.open(
+        method,
+        isGet && !!data
+          ? `${url}${queryStringify(data)}`
+          : url,
+      );
+
+      // Установить таймаут
+      xhr.timeout = timeout;
+
+      // Установить заголовки
+      Object.entries(headers).forEach(([key, value]) => {
+        xhr.setRequestHeader(key, value);
+      });
+
+      xhr.onload = () => resolve(xhr);
+      xhr.onerror = () => reject(new Error('Network Error'));
+      xhr.ontimeout = () => reject(new Error('Request timed out'));
+
+      if (isGet || !data) {
+        xhr.send();
+      } else {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(data);
+      }
+    });
+  };
+}
+
+export default HTTP;
