f06eb79812b5e5e273b53df7ffa7d101a1b5f33d
Maria Shkurko <Angel-of-Death.Maria@yandex.ru>
2025-08-21 23:30:47 +0300

    block component - avatar

---
+++ src/components/avatar/avatar.ts
@@ -0,0 +1,27 @@
+import Block, { type Props } from "../../core/Block";
+
+type TAvatarProps = Props & {
+  avatarUrl?: string;
+  width: string;
+  height: string;
+  className?: string;
+};
+
+export default class Avatar extends Block<TAvatarProps> {
+  constructor(props: TAvatarProps) {
+    super("div", {
+      ...props,
+      className: `avatar${props.className ? ` ${props.className}` : ""}`,
+      style: { "--avatar-width": props.width, "--avatar-height": props.height },
+    });
+  }
+
+  render() {
+    if (this.props.avatarUrl) {
+      return `
+        <img src={{avatarUrl}} alt="avatar" />
+      `;
+    }
+    return "";
+  }
+}
